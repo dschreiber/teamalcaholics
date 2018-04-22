7188c8d17455b3a0556c4dec3c8c6038f0995099
Darren Schreiber <frizzo@computers-macbook-pro.local>
2018-04-22 11:59:10 -0700

    Typo 2

--- ctrl/events/home.php
+++ ctrl/events/home.php
@@ -2,7 +2,7 @@
 
 global $pdo;
 
-$sql = $pdo->prepare("SELECT * FROM event WHERE date >= ?");
+$sql = $pdo->prepare("SELECT * FROM event WHERE event_end >= ?");
 $today = date('m/d/Y');
 $sql->execute([$today]);
 $events = $sql->fetch();
