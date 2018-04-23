d80b244d9815a149c4ca4b70178bbc5e914c6957
root <root@teamalcaholics.com>
2018-04-23 06:49:06 +0000

    Fix title

--- register.php
+++ register.php
@@ -59,7 +59,7 @@ if (isset($_REQUEST['register'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>SB Admin - Start Bootstrap Template</title>
+  <title>Team ALCaholics - Register</title>
   <!-- Bootstrap core CSS-->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
